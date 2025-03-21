5146e76b27c1315b2162c6a18048aa5a5689ed31
Jerry <jerry_agboola@hotmail.com>
2025-03-21 02:46:27 +0100

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,13 @@
 // Your web app's Firebase configuration
+// yes, i know... no creds on git!
 const firebaseConfig = {
+  apiKey: "AIzaSyDSUPgmdfMjXaxCtsevRJK165FaOLDA6RI",
+  authDomain: "starthack2025.firebaseapp.com",
+  databaseURL: "https://starthack2025-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "starthack2025",
+  storageBucket: "starthack2025.firebasestorage.app",
+  messagingSenderId: "438414179401",
+  appId: "1:438414179401:web:7cf2b10966cfdbf4871398"
 };
 
 export default firebaseConfig;
